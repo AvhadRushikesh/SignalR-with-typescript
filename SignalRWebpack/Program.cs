2787604a9ff9c69dac681404da966ace30d3d938
rushi-smb <rushi@smbios.co.in>
2023-03-17 00:21:29 +0530

    allow server to locate and serve index.html file(file serve whether user enters full url or root url of web app)

--- SignalRWebpack/Program.cs
+++ SignalRWebpack/Program.cs
@@ -1,6 +1,11 @@
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddSignalR();
+
 var app = builder.Build();
+
+app.UseDefaultFiles();
+app.UseStaticFiles();
 
 app.MapGet("/", () => "Hello World!");
 
